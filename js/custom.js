d741a673818159df0af0e6848bcf16b1bb7253a4
dineshmehra86 <dinesh.mehra86@gmail.com>
2023-06-19 01:47:39 +0530

    Javascript data type and Typescript introduction

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+// Example of TypeScript (Strict)
+// We cannot excute the typescript file directly because JS/Node only understand js file
+// The Process of TS to JS conversion is called transpilation
+// To compile the TS file into JS file, we need to run npx tsc command (tsc-typescript compiler) 
+var name1 = "Dinesh"; // Declaration and Initialization
+console.log(name1);
+var age; // Decleration
+age = 37;
+console.log(age);
---
+++ js/custom.ts
@@ -0,0 +1,15 @@
+// Example of TypeScript (Strict)
+// We cannot excute the typescript file directly because JS/Node only understand js file
+// The Process of TS to JS conversion is called transpilation
+// To compile the TS file into JS file, we need to run npx tsc command (tsc-typescript compiler) 
+
+var name1:string="Dinesh"; // Declaration and Initialization
+console.log(name1);
+
+
+
+var age:number; // Decleration
+
+age = 37;
+
+console.log(age);
